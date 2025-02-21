d70a66ce0a9f838a14eb3182285adbf3fb666c6c
taronja4 <153595761+taronja4@users.noreply.github.com>
2025-02-21 13:40:48 +0100

    Update Guerrero.java

--- Guerrero.java
+++ Guerrero.java
@@ -1,7 +1,9 @@
+import java.util.ArrayList;
+
 public class Guerrero extends heroes{
     int fuerza;
 
-    public Guerrero(String nombre, int nivel, int HP, arma arma, int fuerza) {
+    public Guerrero(String nombre, int nivel, int HP, ArrayList<arma> arma, int fuerza) {
         super(nombre, nivel, HP, arma);
         this.fuerza = fuerza;
     }
@@ -9,13 +11,15 @@ public class Guerrero extends heroes{
     public int getFuerza() {
         return fuerza;
     }
+
     @Override
     public String toString() {
         return "Guerrero{" +
-                " nombre='" + nombre + '\'' +
+                "nombre='" + nombre + '\'' +
                 ", nivel=" + nivel +
                 ", HP=" + HP +
                 ", fuerza=" + fuerza +
+                ", arma=" + arma +
                 '}';
     }
 }
